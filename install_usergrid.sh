e6e2aad2f4d7a36dbca114de90de0ca806504255
Dave Johnson <snoopdave@apache.org>
2016-01-02 11:28:32 -0500

    Include Usergrid release instead of building it from git cloned sources.

--- install_usergrid.sh
+++ install_usergrid.sh
@@ -16,41 +16,29 @@
 #
 #-------------------------------------------------------------------------------
 
-# purge old Node.js, add repo for new Node.js
+# Purge old Node.js, add repo for new Node.js
 apt-get purge nodejs npm
 apt-add-repository -y ppa:chris-lea/node.js
 
-# install what we need for building and running Usergrid Stack and Portal
+# Install what we need for building and running Usergrid Stack and Portal
 apt-get -y update
 apt-get -y install tomcat7 unzip git maven nodejs python-software-properties python g++ make
 /etc/init.d/tomcat7 stop
 
-# fetch usergrid code in our home dir
-cd /home/vagrant
-git clone https://git-wip-us.apache.org/repos/asf/usergrid.git usergrid
-export USERGRID_PROJECT=/home/vagrant/usergrid
+# Deploy Usergrid stack and portal to Tomcat
+cd /vagrant/usergrid
 
-# build Usergrid stack, deploy it to Tomcat and then configure it
-cd ${USERGRID_PROJECT}
-git checkout master
-
-cd ${USERGRID_PROJECT}/sdks/java
-mvn -DskipTests=true install
-
-cd ${USERGRID_PROJECT}/stack
-mvn -DskipTests=true install
-cp rest/src/test/resources/log4j.properties /usr/share/tomcat7/lib/
-
-cd ${USERGRID_PROJECT}/stack/rest/target
 rm -rf /var/lib/tomcat7/webapps/*
 cp -r ROOT.war /var/lib/tomcat7/webapps
-mkdir -p /usr/share/tomcat7/lib 
 
+mkdir -p /usr/share/tomcat7/lib 
+cp log4j.properties /usr/share/tomcat7/lib/
+
+# Write Usergrid config
 export superUserEmail=superuser@example.com
-export testAdminUserEmail=superuser@example.com
+export testAdminUserEmail=testadmin@example.com
 export baseUrl=http://${PUBLIC_HOSTNAME}:8080
 
-# write Usergrid config
 cd /vagrant
 cat >> /usr/share/tomcat7/lib/usergrid-deployment.properties << EOF
 
@@ -119,30 +107,27 @@ usergrid.admin.resetpw.url=${baseUrl}/management/users/%s/resetpw
 usergrid.user.resetpw.url=${baseUrl}/%s/%s/users/%s/resetpw
 EOF
 
-# configure Tomcat memory and and hook up Log4j because Usergrid uses it 
+# Configure Tomcat memory and and hook up Log4j because Usergrid uses it 
 cd /home/vagrant
 cat >> /usr/share/tomcat7/bin/setenv.sh << EOF
 export JAVA_OPTS="-Xmx450m -Dlog4j.configuration=file:///usr/share/tomcat7/lib/log4j.properties -Dlog4j.debug=false"
 EOF
 chmod +x /usr/share/tomcat7/bin/setenv.sh
-cp ${USERGRID_PROJECT}/stack/rest/src/test/resources/log4j.properties /usr/share/tomcat7/lib/
 
-# build and deploy Usergrid Portal to Tomcat
+# Build and deploy Usergrid Portal to Tomcat
 npm install karma-phantomjs-launcher --save-dev
-
-cd ${USERGRID_PROJECT}/portal
-./build.sh 
-cd dist
-mkdir /var/lib/tomcat7/webapps/portal
-cp -r usergrid-portal/* /var/lib/tomcat7/webapps/portal
+cd /vagrant/usergrid
+tar xzvf usergrid-portal.tgz
+cp -r usergrid-portal /var/lib/tomcat7/webapps/portal
 sed -i.bak "s/http\:\/\/localhost/http\:\/\/${PUBLIC_HOSTNAME}/" /var/lib/tomcat7/webapps/portal/config.js 
+rm -rf usergrid-portal
 
-# go!
+# GO!
 /etc/init.d/tomcat7 start
 
 sleep 10
 
-# init usergrid
+# Init usergrid
 curl -X PUT http://localhost:8080/system/database/setup -u superuser:test
 curl -X PUT http://localhost:8080/system/database/bootstrap -u superuser:test
 curl -X GET http://localhost:8080/system/superuser/setup -u superuser:test
