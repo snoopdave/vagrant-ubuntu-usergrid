6d0af4d4af9f170041d56e87a9b3bbce59a93b9b
Guillermo Arribas <garribas@gmail.com>
2016-02-19 12:09:45 -0300

    Fix issue: add-apt-repository command not available in ubuntu-14.04-amd64-vbox.box

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,6 +25,7 @@ echo "Installing OpenJDK"
 echo "--------------------------------------------------------------------------"
 echo " "
 
+apt-get -y install software-properties-common
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
 apt-get -y install vim curl openjdk-8-jdk 
